8b040fd3891e5331a025b75bbd419c7e9b63f813
ericjuden <ericjuden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-09 20:53:53 +0000

    Initial commit of Weekly Archive widget
    git-svn-id: https://plugins.svn.wordpress.org/weekly-archive-widget/trunk@435913 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Weekly Archive Widget ===
+Contributors: ericjuden
+Tags: archive, archives, week, weekly, weekly archive, widget
+Requires at least 2.8
+Tested up to: 3.2.1
+Stable tag: trunk
+
+== Description ==
+
+Create a widget that displays the archives by week
+
+== Installation ==
+
+1. Copy the plugin files to <code>wp-content/plugins/</code>
+
+2. Activate the plugin from Plugins page
+
+3. Go to Appearance -> Widgets
+
+4. Look for Weekly Archive and drag to appropriate sidebar
+
+== Frequently Asked Questions ==
+
+= How do I change the day of the week the archive starts on? =
+
+Go to Settings -> General and change the value of <b>Week Starts On</b>.
+
+== Screenshots ==
+1. In a sidebar, with the dropdown expanded
+2. The widget settings screen
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ weekly-archive-widget.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Weekly Archive Widget
+Plugin URI: http://judenware.com/projects/wordpress/weekly-archive-widget/
+Version 1.0
+Description: Create a widget that displays the archives by week
+Author: ericjuden
+Author URI: http://www.judenware.com/
+*/
+
+class Weekly_Archive_Widget extends WP_Widget {
+    function Weekly_Archive_Widget(){
+        $widget_ops = array('classname' => 'weekly-archive-widget', 'description' => _('Create a widget that displays the archives by week'));
+        $this->WP_Widget('Weekly_Archive_Widget', _('Weekly Archive'), $widget_ops);
+    }
+    
+    function form($instance){
+        $instance = wp_parse_args((array) $instance, array('title' => ''));
+        $title = $instance['title'];
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
+<?php
+    }
+    
+    function update($new_instance, $old_instance){
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        return $instance;
+    }
+    
+    function widget($args, $instance){
+        extract($args, EXTR_SKIP);
+        
+        echo $before_widget;
+        $title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+        
+        if(!empty($title)){
+            echo $before_title . $title . $after_title;
+        }
+?>
+        <ul>
+			<select name="weekly-archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+		    	<option value=""><?php _e('Select Week'); ?></option>
+		    	<?php wp_get_archives('type=weekly&format=option'); ?>
+		    </select>
+		</ul>
+<?php
+        echo $after_widget;
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Weekly_Archive_Widget");'));
+?>
